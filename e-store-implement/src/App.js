de90c135bbf4d067134488922163eb13d35d2e7e
harshchoudhary981066 <harsh981066@gmail.com>
2023-12-03 14:01:55 +0530

    Check Commit

--- e-store-implement/src/App.js
+++ e-store-implement/src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  cosnt [results, setResults ] = useState([]);
+  useEffect(() => {
+    fetch("http:/localhost:3001/categories")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setResults(data);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
